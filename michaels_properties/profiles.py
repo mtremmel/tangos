fecca28924009b34fe7dafe959e042dfd37a0b9c
Michael <mtremmel@github.com>
2018-01-02 16:45:10 -0500

    fix profile references to reflect pynbody/yt separation in TANGOS

--- michaels_properties/profiles.py
+++ michaels_properties/profiles.py
@@ -1,5 +1,5 @@
 from tangos.properties import HaloProperties, TimeChunkedProperty
-from tangos.properties.spherical_region import SphericalRegionHaloProperties
+from tangos.properties.pynbody.spherical_region import SphericalRegionHaloProperties
 import numpy as np
 import pynbody
 from tangos.properties.centring import centred_calculation
