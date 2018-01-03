6dfb801d379f68f8c96d389d384ce70a7f7a7a78
mtremmel <m.tremmel6@gmail.com>
2018-01-03 10:58:56 -0500

    add escape energy calculation

--- michaels_properties/basic.py
+++ michaels_properties/basic.py
@@ -26,3 +26,34 @@ class Radius(LiveHaloProperties):
 		rho_crit = (3 * H_z ** 2) / (8 * np.pi * pynbody.units.G)
 		rho_mean = halo['tot_mass_profile']/(4./3. * np.pi * ((np.arange(len(halo['tot_mass_profile']))+1)*0.1)**3)
 		return np.where(rho_mean>rho_crit.in_units('Msol kpc**-3')*self._ncrit)[0][-1]*0.1
+
+class EscapeEnergy(LiveHaloProperties):
+
+	name='escape_energy_profile'
+
+	def requires_property(self):
+		return ['tot_mass_profile']
+
+	def live_calculate(self,halo):
+		mass = np.asarray(sim.timesteps[ii].halos[0]['tot_mass_profile'])
+
+		max_x = len(mass) * 0.1
+		pot_dx = 0.01
+
+		r = np.arange(0.05, 0.04 + 0.1 * len(mass), 0.1)
+		rnew = np.arange(pot_dx, max_x, pot_dx)
+
+		min_r_i = int(0.35 / pot_dx)
+
+		mass_new = np.ones(len(rnew))
+
+		import scipy
+		mass_new[min_r_i:] = scipy.interp(rnew[min_r_i:], r, mass)
+		mass_new[:min_r_i] = mass_new[min_r_i] * (rnew[:min_r_i] / rnew[min_r_i]) ** 3
+
+		force = pynbody.units.G * mass_new / (rnew ** 2)
+
+
+		pot = np.cumsum(pot_dx * force)
+		pot_diff = pot[-1] - pot[::10]
+		return pot_diff.in_units('cm**2 s**-2')
