38429f964ee9393f9d8647c16aa8dcfe3312161a
Michael <mtremmel@github.com>
2018-02-13 16:24:39 -0500

    new flow calculation that targets outflows more explicitly

--- michaels_properties/flows.py
+++ michaels_properties/flows.py
@@ -59,3 +59,69 @@ class NewFlowProfile(SphericalRegionHaloProperties):
             #outflow_Mdot_dm, outflow_vel_dm, outflow_vel2_dm, _ = self.profile_calculation(halo.ancestor.dm, self._threshold_vel,properties['max_radius'])
         halo.ancestor['pos'] = original_positions
         return inflow_Mdot, outflow_Mdot, -inflow_vel, outflow_vel,  inflow_vel2, outflow_vel2, inflow_temp, outflow_temp
+
+
+def get_outflow_particles(self,fgas):
+    med_v = np.median(fgas['vr'])
+    mad_v = np.mean(np.abs(fgas['vr']-np.median(fgas['vr'])))
+    if med_v > mad_v:
+        return (fgas['vr']>self._threshold_vel).view(np.ndarray)
+    else:
+        return (fgas['vr']>max(self._threshold_vel,med_v+mad_v)).view(np.ndarray)
+
+
+@pynbody.analysis.profile.Profile.profile_property
+def outflow(self,data='mdot'):
+    out = np.zeros(self.nbins)
+    for i in range(self.nbins):
+        subs = self.sim[self.binind[i]]
+        f_gas = subs.g
+        mdot = f_gas['mass'] * f_gas['vr'] / (pynbody.units.Unit("kpc")*self.plot_xdelta())
+        mdot *= get_outflow_particles(f_gas)
+        if data=='mdot':
+            out[i] = np.sum(mdot.in_units("Msol yr^-1"))
+        else:
+            out[i] = np.sum(mdot*f_gas[data])/np.sum(mdot)
+    return out
+
+
+class WindProfile(SphericalRegionHaloProperties):
+
+    _threshold_vel = 20.0
+    names = "new_gas_outflow_mdot", "new_gas_outflow_v", "new_gas_outflow_v2","new_gas_outflow_temp",
+    def region_specification(self, db_data):
+        return pynbody.filt.Sphere(db_data['max_radius'], db_data['shrink_center']) & \
+               (pynbody.filt.FamilyFilter(pynbody.family.gas)|pynbody.filt.FamilyFilter(pynbody.family.star))
+
+    def plot_x0(cls):
+        return 0.5
+
+    @classmethod
+    def plot_xdelta(cls):
+        return 1.0
+
+    def get_outflow_particles(self,fgas):
+        med_v = np.median(fgas['vr'])
+        mad_v = np.median(np.abs(fgas-np.median(fgas['vr_mean'])))
+        if med_v > mad_v:
+            return (fgas['vr']>self._threshold_vel).view(np.ndarray)
+        else:
+            return (fgas['vr']>max(self._threshold_vel,med_v+mad_v)).view(np.ndarray)
+
+
+    def profile_calculation(self, f_gas, rvir):
+        pro = pynbody.analysis.profile.Profile(f_gas, min=0.0,max=rvir,
+                                               nbins=int(rvir/self.plot_xdelta()), ndim=3)
+        return pro['outflow, mdot'], pro['outflow, vr'], pro['outflow,vr2'], pro['outflow,temp']
+
+    def calculate(self, halo, properties):
+        original_positions = np.array(halo.ancestor['pos'])
+        original_velocities = np.array(halo.ancestor['vel'])
+        halo.ancestor['pos'] -= properties['shrink_center']
+        halo.ancestor.wrap()
+        with pynbody.analysis.halo.vel_center(halo):
+            halo.gas['vr2'] = halo.gas['vr'] ** 2
+            outflow_Mdot, outflow_vel, outflow_vel2, outflow_temp = self.profile_calculation(halo.ancestor.gas,properties['max_radius'])
+        halo.ancestor['pos'] = original_positions
+        halo.ancestor['vel'] = original_velocities
+        return outflow_Mdot, outflow_vel, outflow_vel2, outflow_temp
